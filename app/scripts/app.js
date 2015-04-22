e199810b28b0ccad7156a2c5b287d0696e457ddc
fer <vilasmaciel@gmail.com>
2015-04-22 18:40:55 +0200

    Working on create a unique elemet to manage all the app's logic

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,12 +4,13 @@
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
+  //var app = document.querySelector('#app');
+
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('template-bound', function() {
+  document.addEventListener('template-bound', function() {
+    console.log(app);
     console.log('Our app is ready to rock!');
   });
 
